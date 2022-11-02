8b6f9a6fcb9370f9e3b8ca1e32e364104093becd
Hendra Dwi Saputra <hendra@usa.com>
2022-11-02 20:55:03 +0700

    Get data and parse into ip:port format

--- makelar/spiders/proxy.py
+++ makelar/spiders/proxy.py
@@ -6,5 +6,7 @@ class ProxySpider(scrapy.Spider):
     allowed_domains = ['free-proxy-list.net']
     start_urls = ['http://free-proxy-list.net/']
 
-    def parse(self, response):
-        pass
+    def parse(self, response, **kwargs):
+        tr = response.xpath('//div[@class="table-responsive fpl-list"]/table/tbody/tr')
+        for row in tr:
+            print("{}:{}".format(row.xpath('td/text()')[0].extract(), row.xpath('td/text()')[1].extract()))
