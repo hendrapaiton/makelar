de338cdfe5c5655955f5f605477964d4a4d0e263
Hendra Dwi Saputra <hendra@usa.com>
2022-11-03 17:30:19 +0700

    Make scrapy pipelines to get data and save to text files

--- makelar/pipelines.py
+++ makelar/pipelines.py
@@ -1,13 +1,12 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-
-
 class MakelarPipeline:
+    file = None
+
+    def open_spider(self, spider):
+        self.file = open('proxy.txt', 'w')
+
+    def close_spider(self, spider):
+        self.file.close()
+
     def process_item(self, item, spider):
+        self.file.write('{}:{}'.format(item['addr'], item['port']))
         return item
--- makelar/spiders/proxy.py
+++ makelar/spiders/proxy.py
@@ -1,5 +1,7 @@
 import scrapy
 
+from makelar.items import MakelarItem
+
 
 class ProxySpider(scrapy.Spider):
     name = 'proxy'
@@ -7,6 +9,9 @@ class ProxySpider(scrapy.Spider):
     start_urls = ['http://free-proxy-list.net/']
 
     def parse(self, response, **kwargs):
+        items = MakelarItem()
         tr = response.xpath('//div[@class="table-responsive fpl-list"]/table/tbody/tr')
         for row in tr:
-            print("{}:{}".format(row.xpath('td/text()')[0].extract(), row.xpath('td/text()')[1].extract()))
+            items['addr'] = row.xpath('td/text()')[0].extract()
+            items['port'] = row.xpath('td/text()')[1].extract()
+            yield items
